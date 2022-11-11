23e46932790d94284617cdfb8845eef5bedff6c3
Tunay Bektas <what05@web.de>
2022-11-11 16:27:22 +0100

    Card - added styling -fixed some issued

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,12 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 import Card from "../components/card/Card.js";
 
 function Activities() {
   return (
     <>
-      <Card />
+      <Position>
+        <Card />
+      </Position>
     </>
   );
 }
+
+const Position = styled.div`
+  text-align: center;
+  margin: 0;
+  background-color: rgb(44, 41, 41);
+  height: 100vh;
+  width: 100vw;
+`;
 
 export default Activities;
